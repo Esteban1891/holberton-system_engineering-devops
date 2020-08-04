51037527de54b5c5531569bb964f70c19fcbcda8
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-03 22:20:13 -0500

    add all

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# create a file in /tmp.
+# Creating a file
 file { 'holberton':
-	path            =>  '/tmp/holberton',
-	mode            =>  '0744',
-	owner           =>  'www-data',
-	group           =>  'www-data',
-	content         =>  'I love Puppet',
+  path    => '/tmp/holberton',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
 }
