80db79ed603cb3d03ed6f7c37371c9821c139fa6
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-14 20:48:22 -0500

    add tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
-"""
-Requests an API with to get data about all employees
-Returns information about the employees TODO list in JSON format
-"""
-if __name__ == "__main__":
-    from collections import OrderedDict
-    import json
-    import requests
+"""Python script to export data in the JSON format"""
 
-    url = 'https://jsonplaceholder.typicode.com/'
-    employees_url = url + 'users'
-    employees = requests.get(employees_url).json()
-    json_file = 'todo_all_employees.json'
-    with open(json_file, 'w') as f:
-        employees_dict = OrderedDict()
-        for employee in employees:
-            employee_id = employee.get('id')
-            employee_name = employee.get('name')
-            todo_url = url + 'todos?userId={}'.format(employee_id)
-            todo = requests.get(todo_url).json()
-            todo_list = []
-            for task in todo:
-                task_title = task.get('title')
-                task_completed = task.get('completed')
-                todo_dict = [("username", employee_name), ("task", task_title),
-                             ("completed", task_completed)]
-                todo_dict = OrderedDict(todo_dict)
-                todo_list.append(todo_dict)
-            employees_dict[str(employee_id)] = todo_list
-        json.dump(employees_dict, f)
+import json
+import requests
+import sys
+
+base_url = 'https://jsonplaceholder.typicode.com/'
+
+
+def do_request():
+    '''Performs request'''
+    response = requests.get(base_url + 'users/')
+    if response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    users = response.json()
+
+    response = requests.get(base_url + 'todos/')
+    if response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    todos = response.json()
+
+    data = {}
+    for user in users:
+        user_todos = [todo for todo in todos
+                      if todo.get('userId') == user.get('id')]
+        user_todos = [{'username': user.get('username'),
+                       'task': todo.get('title'),
+                       'completed': todo.get('completed')}
+                      for todo in user_todos]
+        data[str(user.get('id'))] = user_todos
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(data, file)
+
+
+if __name__ == '__main__':
+    do_request()
