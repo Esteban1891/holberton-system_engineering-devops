e95389d3948ae4b8e42e6c8a554427f2ee09bbef
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-30 19:47:38 -0500

    aaaaaa

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Fixes an error with a WordPress Website
 exec { '/var/www/html/wp-setting.php':
   command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
