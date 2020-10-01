b7cb2c99f7ddc6b77f4d285eb30219109807a726
Esteban1891 <esteban.delahoz15@gmail.com>
2020-09-30 19:41:58 -0500

    add readme

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,4 @@
+exec { '/var/www/html/wp-setting.php':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
