7275a9ead328b4f78f38cf77ffba4b92c11acfe9
Esteban <60367971+Esteban1891@users.noreply.github.com>
2020-10-08 15:41:25 -0500

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,10 +4,10 @@
 exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-}
+} ->
 
 # Restart Nginx
 exec { 'nginx-restart':
-  command => 'sudo service nginx restart',
+  command => 'nginx restart',
   path    => '/etc/init.d/'
 }
