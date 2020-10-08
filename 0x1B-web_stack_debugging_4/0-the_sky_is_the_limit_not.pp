fc3b8f0714f1cb6f7ef5bdc029be6399b85ffe70
Esteban1891 <esteban.delahoz15@gmail.com>
2020-10-08 15:37:29 -0500

    add solution, change number in nginx default

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Increases the amount of traffic an Nginx server can handle.
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'sudo service nginx restart',
+}
