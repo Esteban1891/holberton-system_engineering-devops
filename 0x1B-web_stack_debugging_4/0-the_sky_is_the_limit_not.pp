59a21f3b684a556fd47fc46b3bd26c1ce091c852
Esteban <60367971+Esteban1891@users.noreply.github.com>
2020-10-08 15:47:02 -0500

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,9 +3,7 @@
 # Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
   command => 'sed -i "s/ULIMIT="-n 15"/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
 }
-
 # Restart Nginx
 exec { 'nginx-restart':
   command => 'nginx restart'
